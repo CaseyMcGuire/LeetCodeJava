8c19216d150082db5f61a1c857598e752e804fdd
caseymcguire <caseyjaymcguire@gmail.com>
2018-03-29 19:35:59 -0700

    Finished problem 108.

---
+++ src/main/com/leetcode/problems/problem0108/Solution.java
@@ -0,0 +1,27 @@
+package main.com.leetcode.problems.problem0108;
+
+import main.com.leetcode.datastructures.TreeNode;
+
+
+public class Solution {
+  public TreeNode sortedArrayToBST(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return null;
+    }
+    TreeNode tree = sortedArrayToBST(nums, 0, nums.length - 1);
+    return tree;
+  }
+
+  private TreeNode sortedArrayToBST(int[] nums, int from, int to) {
+    if (from > to) {
+      return null;
+    }
+    int middle = (from + to) / 2;
+    TreeNode node = new TreeNode(nums[middle]);
+    if (from != to) {
+      node.left = sortedArrayToBST(nums, from, middle - 1);
+      node.right = sortedArrayToBST(nums, middle + 1, to);
+    }
+    return node;
+  }
+}
